9d3457a2b70aaaf7c035561cf918a8cec8b4a0b8
VCarlosJP <carlosjp.villanueva@gmail.com>
2020-10-13 19:32:17 +0200

    react-router-dom implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import Login from './components/Login'
+import Expenses from './components/Expenses'
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './App.scss';
 
@@ -8,8 +10,12 @@ import './App.scss';
 
 function App() {
   return (
-    <Login/>
-    
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login}></Route>
+        <Route path="/expenses" component={Expenses}></Route>
+      </Switch>
+    </Router>
   );
 }
 
