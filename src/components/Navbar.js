b085298e86c504e151510e7c35f89331c4d41d67
VCarlosJP <carlosjp.villanueva@gmail.com>
2020-10-14 16:45:44 +0200

    navbar minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEuroSign, faTasks, faTools } from "@fortawesome/free-solid-svg-icons";
+import { faClipboard, faEuroSign, faTasks, faTools, faCreditCard } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar(){
     return (
         <div className="navbar-container">
                    
-        <FontAwesomeIcon icon={faEuroSign} className="button-icon" />
+        <FontAwesomeIcon icon={faCreditCard} className="button-icon" />
     
         <FontAwesomeIcon icon={faTasks} className="button-icon" />
     
