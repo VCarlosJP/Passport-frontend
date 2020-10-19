a7b466c0155d274431d71191d5e503bb5167a06f
VCarlosJP <carlosjp.villanueva@gmail.com>
2020-10-19 22:40:16 +0200

    Operations: Operations header now its a separate component to be loaded with the threee main transactions

---
+++ src/components/Operations.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Operations(){
+    return (
+        
+                <div className="content-header">
+                    <div className="dimelo">
+                    <h1 className="title is-1 content-title">Operations</h1>
+                        <ul className="options-list">
+                            <li className="active-option">Expenses</li>
+                            <li>Debts</li>
+                            <li>Graphs</li>
+                        </ul>
+                    </div>
+                    <h2 className="title is-2 testing">€1,024.00</h2>
+                </div>
+        
+    )
+}
